62554671a8cf19c82493600790f80f19322cc6d1
safiqq <safiqq@metavault.dev>
2025-04-28 10:00:00 +0700

    feat: create settings pages

---
+++ app/settings/accountsecurity.tsx
@@ -0,0 +1,300 @@
+import { Image, Platform, Pressable, View } from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedText } from "@/components/ThemedText";
+import React, { useState } from "react";
+import { Switch } from "@/components/ui/Switch";
+import { Line } from "@/components/ui/Line";
+import Spacer from "@/components/Spacer";
+import { DropdownMenu } from "@/components/ui/DropdownMenu";
+import { MenuOption } from "@/components/ui/MenuOption";
+
+export default function AccountSecurityScreen() {
+  const [accountSecurityStates, setAccountSecurityStates] = useState({
+    unlockFingerprint: true,
+    unlockFaceId: false,
+    unlockSqrl: true,
+    sessionTimeout: "15 minutes",
+    sessionTimeoutAction: "Lock",
+  });
+  const [sessionTimeoutVisible, setSessionTimeoutVisible] = useState(false);
+  const [sessionTimeoutActionVisible, setSessionTimeoutActionVisible] =
+    useState(false);
+
+  return (
+    <SafeAreaView
+      className={`flex-1 w-full ${Platform.OS == "web" && "max-w-2xl mx-auto"}`}
+    >
+      
+      <View className="mx-6 my-5 gap-4">
+        <View>
+          <ThemedText fontSize={12} fontWeight={800}>
+            UNLOCK OPTIONS
+          </ThemedText>
+
+          <Spacer size={4} />
+
+          <View className="bg-[#EBEBEB] rounded-lg px-4 py-3">
+            <View className="gap-2">
+              <View className="flex flex-row items-center justify-between">
+                <ThemedText fontSize={14}>Unlock with fingerprint</ThemedText>
+                <Switch
+                  state={accountSecurityStates.unlockFingerprint}
+                  callback={() =>
+                    setAccountSecurityStates({
+                      ...accountSecurityStates,
+                      unlockFingerprint:
+                        !accountSecurityStates.unlockFingerprint,
+                    })
+                  }
+                />
+              </View>
+
+              <Line />
+
+              <View className="flex flex-row items-center justify-between">
+                <ThemedText fontSize={14}>Unlock with FaceID</ThemedText>
+                <Switch
+                  state={accountSecurityStates.unlockFaceId}
+                  callback={() =>
+                    setAccountSecurityStates({
+                      ...accountSecurityStates,
+                      unlockFaceId: !accountSecurityStates.unlockFaceId,
+                    })
+                  }
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+        <View>
+          <ThemedText fontSize={12} fontWeight={800}>
+            SESSION TIMEOUT
+          </ThemedText>
+
+          <Spacer size={4} />
+
+          <View className="bg-[#EBEBEB] rounded-lg px-4 py-3">
+            <View className="gap-2">
+              <View>
+                <ThemedText fontSize={12} fontWeight={800}>
+                  Session timeout
+                </ThemedText>
+                <DropdownMenu
+                  visible={sessionTimeoutVisible}
+                  handleOpen={() => setSessionTimeoutVisible(true)}
+                  handleClose={() => setSessionTimeoutVisible(false)}
+                  trigger={
+                    <View className="flex flex-row justify-between items-center">
+                      <ThemedText fontSize={14}>
+                        {accountSecurityStates.sessionTimeout}
+                      </ThemedText>
+                      <Image
+                        className="max-w-4 max-h-4 -mt-1"
+                        source={require("@/assets/images/arrow-down.png")}
+                      />
+                    </View>
+                  }
+                >
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "Immediately",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      Immediately
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "1 minute",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      1 minute
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "5 minutes",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      5 minutes
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "15 minutes",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      15 minutes
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "30 minutes",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      30 minutes
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "1 hour",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      1 hour
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "4 hours",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      4 hours
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "On app restart",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      On app restart
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeout: "Never",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      Never
+                    </ThemedText>
+                  </MenuOption>
+                </DropdownMenu>
+              </View>
+
+              <Line />
+
+              <View>
+                <ThemedText fontSize={12} fontWeight={800}>
+                  Session timeout action
+                </ThemedText>
+                <DropdownMenu
+                  visible={sessionTimeoutActionVisible}
+                  handleOpen={() => setSessionTimeoutActionVisible(true)}
+                  handleClose={() => setSessionTimeoutActionVisible(false)}
+                  trigger={
+                    <View className="flex flex-row justify-between items-center">
+                      <ThemedText fontSize={14}>
+                        {accountSecurityStates.sessionTimeoutAction}
+                      </ThemedText>
+                      <Image
+                        className="max-w-4 max-h-4 -mt-1"
+                        source={require("@/assets/images/arrow-down.png")}
+                      />
+                    </View>
+                  }
+                >
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutActionVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeoutAction: "Lock",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      Lock
+                    </ThemedText>
+                  </MenuOption>
+                  <MenuOption
+                    onSelect={() => {
+                      setSessionTimeoutActionVisible(false);
+                      setAccountSecurityStates({
+                        ...accountSecurityStates,
+                        sessionTimeoutAction: "Log out",
+                      });
+                    }}
+                  >
+                    <ThemedText fontSize={14} className="text-white">
+                      Log out
+                    </ThemedText>
+                  </MenuOption>
+                </DropdownMenu>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View>
+          <ThemedText fontSize={12} fontWeight={800}>
+            OTHER
+          </ThemedText>
+
+          <Spacer size={4} />
+
+          <View className="bg-[#EBEBEB] rounded-lg px-4 py-3">
+            <View className="gap-2">
+              <ThemedText fontSize={14}>Devices</ThemedText>
+
+              <Line />
+
+              <ThemedText fontSize={14}>Lock now</ThemedText>
+
+              <Line />
+
+              <ThemedText fontSize={14}>Log out</ThemedText>
+
+              <Line />
+
+              <ThemedText fontSize={14}>Delete account</ThemedText>
+            </View>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
---
+++ app/settings/other.tsx
@@ -0,0 +1,87 @@
+import { Image, Platform, Pressable, View } from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedText } from "@/components/ThemedText";
+import React, { useState } from "react";
+import Spacer from "@/components/Spacer";
+
+export default function OtherScreen() {
+  const [isSync, setIsSync] = useState(true);
+
+  return (
+    <SafeAreaView
+      className={`flex-1 w-full ${Platform.OS == "web" && "max-w-2xl mx-auto"}`}
+    >
+      <View className="p-6 border-b border-[#EBEBEB] flex flex-row items-center">
+        <View className="flex-1">
+          <Pressable
+            className="flex flex-row items-center"
+            onPress={() => {
+              // return to Settings
+            }}
+          >
+            <Image
+              className="max-w-6 max-h-6"
+              source={require("@/assets/images/arrow-left.png")}
+            />
+            <ThemedText
+              fontSize={10}
+              fontWeight={600}
+              className="text-[#0099FF] -ml-1"
+            >
+              Settings
+            </ThemedText>
+          </Pressable>
+        </View>
+        <View className="flex-1">
+          <ThemedText fontSize={20} fontWeight={700} className="text-center">
+            Other
+          </ThemedText>
+        </View>
+        <View className="flex-1" />
+      </View>
+      <View className="mx-6 my-5 gap-4">
+        <View className="bg-[#EBEBEB] rounded-lg px-4 py-3">
+          <ThemedText fontSize={12} fontWeight={800}>
+            Last sync
+          </ThemedText>
+
+          <Spacer size={4} />
+
+          <ThemedText fontSize={14}>29/04/2025 08.35 UTC</ThemedText>
+          <ThemedText fontSize={12}>(5 seconds ago)</ThemedText>
+
+          <Spacer size={8} />
+
+          <Pressable onPress={() => setIsSync(!isSync)}>
+            {isSync ? (
+              <View className="flex flex-row items-center justify-center gap-2 rounded-full bg-[#BBBBBB] py-2">
+                <Image
+                  className="max-w-4 max-h-4"
+                  source={require("@/assets/images/refresh-2.png")}
+                />
+                <ThemedText
+                  fontSize={14}
+                  fontWeight={700}
+                  className="text-white text-center"
+                >
+                  Currently syncing...
+                </ThemedText>
+              </View>
+            ) : (
+              <View className="rounded-full bg-black py-2">
+                <ThemedText
+                  fontSize={14}
+                  fontWeight={700}
+                  className="text-white text-center"
+                >
+                  Sync now
+                </ThemedText>
+              </View>
+            )}
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
