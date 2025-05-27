d97f0ed80a78f76e4fab9e1ed5011bdb9e19835e
safiqq <safiqq@metavault.dev>
2025-05-27 10:00:00 +0700

    chore: separate types from db types

--- database.types.ts
+++ database.types.ts
@@ -4,318 +4,368 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
+  // Allows to automatically instanciate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "12.2.3 (519615d)"
+  }
   public: {
     Tables: {
+      auth_challenges: {
+        Row: {
+          challenge: string
+          created_at: string
+          expired_at: string
+          user_id: string
+        }
+        Insert: {
+          challenge: string
+          created_at?: string
+          expired_at: string
+          user_id: string
+        }
+        Update: {
+          challenge?: string
+          created_at?: string
+          expired_at?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
+      emails: {
+        Row: {
+          created_at: string
+          email: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          email: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          email?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
       folders: {
         Row: {
-          created_at: string;
-          id: string;
-          name: string;
-          user_id: string;
-        };
+          created_at: string
+          id: string
+          name: string
+          user_id: string
+        }
         Insert: {
-          created_at?: string;
-          id?: string;
-          name: string;
-          user_id: string;
-        };
+          created_at?: string
+          id?: string
+          name: string
+          user_id: string
+        }
         Update: {
-          created_at?: string;
-          id?: string;
-          name?: string;
-          user_id?: string;
-        };
-        Relationships: [
-          {
-            foreignKeyName: "folders_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
-          }
-        ];
-      };
+          created_at?: string
+          id?: string
+          name?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
       logins: {
         Row: {
-          created_at: string;
-          folder_id: string;
-          id: string;
-          is_deleted: boolean;
-          item_name: string;
-          password_hashed: string;
-          user_id: string;
-          username_hashed: string;
-          website: string;
-        };
+          created_at: string
+          deleted_at: string | null
+          encrypted_payload: string
+          folder_id: string
+          id: string
+          updated_at: string
+          user_id: string
+        }
         Insert: {
-          created_at?: string;
-          folder_id?: string;
-          id?: string;
-          is_deleted?: boolean;
-          item_name: string;
-          password_hashed: string;
-          user_id: string;
-          username_hashed: string;
-          website: string;
-        };
+          created_at?: string
+          deleted_at?: string | null
+          encrypted_payload: string
+          folder_id: string
+          id?: string
+          updated_at?: string
+          user_id: string
+        }
         Update: {
-          created_at?: string;
-          folder_id?: string;
-          id?: string;
-          is_deleted?: boolean;
-          item_name?: string;
-          password_hashed?: string;
-          user_id?: string;
-          username_hashed?: string;
-          website?: string;
-        };
+          created_at?: string
+          deleted_at?: string | null
+          encrypted_payload?: string
+          folder_id?: string
+          id?: string
+          updated_at?: string
+          user_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "logins_folder_id_fkey";
-            columns: ["folder_id"];
-            isOneToOne: false;
-            referencedRelation: "folders";
-            referencedColumns: ["id"];
+            foreignKeyName: "logins_e_folder_id_fkey"
+            columns: ["folder_id"]
+            isOneToOne: false
+            referencedRelation: "folders"
+            referencedColumns: ["id"]
           },
-          {
-            foreignKeyName: "logins_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
-          }
-        ];
-      };
-      sqrl_devices: {
+        ]
+      }
+      passkeys: {
         Row: {
-          created_at: string;
-          email: string | null;
-          id: number;
-          is_active: boolean | null;
-          platform: string | null;
-          public_key: string | null;
-        };
+          counter: number
+          created_at: string
+          credential_id: string
+          public_key: Json
+          transports: Json[]
+          user_id: string
+        }
         Insert: {
-          created_at?: string;
-          email?: string | null;
-          id?: number;
-          is_active?: boolean | null;
-          platform?: string | null;
-          public_key?: string | null;
-        };
+          counter: number
+          created_at?: string
+          credential_id: string
+          public_key: Json
+          transports: Json[]
+          user_id: string
+        }
         Update: {
-          created_at?: string;
-          email?: string | null;
-          id?: number;
-          is_active?: boolean | null;
-          platform?: string | null;
-          public_key?: string | null;
-        };
-        Relationships: [];
-      };
+          counter?: number
+          created_at?: string
+          credential_id?: string
+          public_key?: Json
+          transports?: Json[]
+          user_id?: string
+        }
+        Relationships: []
+      }
+      secrets: {
+        Row: {
+          created_at: string
+          hashed_secret: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          hashed_secret: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          hashed_secret?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
+      sessions: {
+        Row: {
+          created_at: string
+          device_id: string
+          device_name: string
+          ip_address: string
+          last_active_at: string
+          revoked_at: string | null
+          user_agent: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          device_id: string
+          device_name: string
+          ip_address: string
+          last_active_at?: string
+          revoked_at?: string | null
+          user_agent: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          device_id?: string
+          device_name?: string
+          ip_address?: string
+          last_active_at?: string
+          revoked_at?: string | null
+          user_agent?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
       ssh_keys: {
         Row: {
-          created_at: string;
-          fingerprint_hashed: string;
-          folder_id: string;
-          id: string;
-          is_deleted: boolean;
-          item_name: string;
-          private_key_hashed: string;
-          public_key_hashed: string;
-          user_id: string;
-        };
+          created_at: string
+          deleted_at: string | null
+          encrypted_payload: string
+          fingerprint: string
+          folder_id: string
+          id: string
+          updated_at: string
+          user_id: string
+        }
         Insert: {
-          created_at?: string;
-          fingerprint_hashed: string;
-          folder_id?: string;
-          id?: string;
-          is_deleted?: boolean;
-          item_name: string;
-          private_key_hashed: string;
-          public_key_hashed: string;
-          user_id: string;
-        };
+          created_at?: string
+          deleted_at?: string | null
+          encrypted_payload: string
+          fingerprint: string
+          folder_id: string
+          id?: string
+          updated_at?: string
+          user_id: string
+        }
         Update: {
-          created_at?: string;
-          fingerprint_hashed?: string;
-          folder_id?: string;
-          id?: string;
-          is_deleted?: boolean;
-          item_name?: string;
-          private_key_hashed?: string;
-          public_key_hashed?: string;
-          user_id?: string;
-        };
+          created_at?: string
+          deleted_at?: string | null
+          encrypted_payload?: string
+          fingerprint?: string
+          folder_id?: string
+          id?: string
+          updated_at?: string
+          user_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "ssh_keys_folder_id_fkey";
-            columns: ["folder_id"];
-            isOneToOne: false;
-            referencedRelation: "folders";
-            referencedColumns: ["id"];
+            foreignKeyName: "ssh_keys_e_folder_id_fkey"
+            columns: ["folder_id"]
+            isOneToOne: false
+            referencedRelation: "folders"
+            referencedColumns: ["id"]
           },
-          {
-            foreignKeyName: "ssh_keys_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
-          }
-        ];
-      };
-      users: {
-        Row: {
-          created_at: string;
-          id: string;
-          user_id: string;
-        };
-        Insert: {
-          created_at?: string;
-          id: string;
-          user_id: string;
-        };
-        Update: {
-          created_at?: string;
-          id?: string;
-          user_id?: string;
-        };
-        Relationships: [];
-      };
-    };
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type DefaultSchema = Database[Extract<keyof Database, "public">];
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
     }
     ? R
     : never
   : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-      DefaultSchema["Views"])
-  ? (DefaultSchema["Tables"] &
-      DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-      Row: infer R;
-    }
-    ? R
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
     : never
-  : never;
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-  ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-      Insert: infer I;
-    }
-    ? I
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never;
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-  ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-      Update: infer U;
-    }
-    ? U
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never;
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
-> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
-  ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-  : never;
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never
-> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-  ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-  : never;
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
 
 export const Constants = {
   public: {
     Enums: {},
   },
-} as const;
-
-export type FolderRow = Database["public"]["Tables"]["folders"]["Row"];
-export type LoginRow = Database["public"]["Tables"]["logins"]["Row"];
-export type SshKeyRow = Database["public"]["Tables"]["ssh_keys"]["Row"];
-
-export type VaultItem =
-  | (LoginRow & { item_type: "login"; folder_name: string })
-  | (SshKeyRow & { item_type: "ssh_key"; folder_name: string });
+} as const
--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,95 @@
-export enum AvailableAuthMethods {
-  FINGERPRINT = 1,
-  FACE_AUTHENTICATION = 2,
-  FACE_ID = 2,
-  SQRL = 4,
-  SECURITY_KEY = 8,
+import { Database } from "@/database.types";
+
+type Folders = Database["public"]["Tables"]["folders"];
+type Logins = Database["public"]["Tables"]["logins"];
+type SshKeys = Database["public"]["Tables"]["ssh_keys"];
+type Emails = Database["public"]["Tables"]["emails"];
+type Secrets = Database["public"]["Tables"]["secrets"];
+
+export type FoldersRow = Folders["Row"];
+export type LoginsRow = Logins["Row"];
+export type SshKeysRow = SshKeys["Row"];
+export type EmailsRow = Emails["Row"];
+export type SecretsRow = Secrets["Row"];
+
+export type FoldersInsert = Folders["Insert"];
+export type LoginsInsert = Logins["Insert"];
+export type SshKeysInsert = SshKeys["Insert"];
+export type EmailsInsert = Emails["Insert"];
+export type SecretsInsert = Secrets["Insert"];
+
+export type FoldersUpdate = Folders["Update"];
+export type LoginsUpdate = Logins["Update"];
+export type SshKeysUpdate = SshKeys["Update"];
+export type EmailsUpdate = Emails["Update"];
+export type SecretsUpdate = Secrets["Update"];
+
+type CredentialRow =
+  | (LoginsRow & { item_type: "login"; folder_name: string })
+  | (SshKeysRow & { item_type: "ssh_key"; folder_name: string });
+
+export interface DecryptedLoginItem extends LoginsInsert {
+  item_name: string;
+  username: string;
+  password: string;
+  website?: string;
+}
+
+export interface DecryptedSSHKeyItem extends SshKeysInsert {
+  item_name: string;
+  public_key: string;
+  private_key: string;
+}
+
+export interface CredentialItem {
+  folder_id: string;
+  folder_name: string;
+  item_name: string;
+
+  username: string;
+  password: string;
+  website: string;
+
+  fingerprint: string;
+  public_key: string;
+  private_key: string;
+}
+
+export enum APP_STATES {
+  NOT_LOGGED_IN = 0,
+  LOGGED_IN = 1,
+  CREATE_ACCOUNT_WAIT_FOR_OTP = 2,
+  CREATE_ACCOUNT_OTP_VERIFIED = 3,
+  CREATE_ACCOUNT_PASSKEY_VERIFIED = 4,
+  CREATE_ACCOUNT_SEED_PHRASE_GENERATED = 5,
+  CREATE_ACCOUNT_SEED_PHRASE_VERIFIED = 6,
+  CREATE_ACCOUNT_SUCCEED = 7,
+  LOGGED_IN_NEED_SESSION_RENEWAL = 8,
+  LOGGED_IN_NEED_SEED_PHRASE_VERIFICATION = 9,
+}
+
+interface GeneratorRow {
+  text: string;
+  createdAt: string;
+}
+
+export interface StoreStates {
+  currentState: APP_STATES;
+
+  email: string;
+  emailVerified: boolean;
+  name: string;
+  mnemonic: string[];
+  mnemonicVerified: boolean;
+  passkeyVerified: boolean;
+
+  generatorData: GeneratorRow[];
+  vaultData: CredentialRow[];
+
+  lastMnemonicVerification: string;
+  lastSessionRenewal: string;
+
+  askMnemonicEvery: number; // in days
+  sessionTimeout: number; // in seconds
+  sessionTimeoutAction: "Lock" | "Log out";
 }
