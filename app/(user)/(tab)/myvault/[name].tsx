759eed6af6ae1bbfbb9a1317aedf8abb3bf2c263
safiqq <safiqq@metavault.dev>
2025-07-07 10:00:00 +0700

    fix: myvault subpage

--- app/(user)/(tab)/myvault/[name].tsx
+++ app/(user)/(tab)/myvault/[name].tsx
@@ -1,44 +1,46 @@
-import { Image, Platform, Pressable, ScrollView, View } from "react-native";
+import { Platform, Pressable, ScrollView, View } from "react-native";
 
-import { SafeAreaView } from "react-native-safe-area-context";
-import { ThemedText } from "@/components/ThemedText";
+import { MoreIcon } from "@/assets/images/icons";
 import Spacer from "@/components/Spacer";
-import { Line } from "@/components/ui/Line";
-import { useState } from "react";
+import { ThemedText } from "@/components/ThemedText";
 import { Header } from "@/components/ui/Header";
+import { Line } from "@/components/ui/Line";
 import { useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function VaultScreen() {
-  const { category } = useLocalSearchParams<{ category: string }>();
+export default function MyVaultNameScreen() {
+  const insets = useSafeAreaInsets();
+
+  const { name } = useLocalSearchParams<{ name: string }>();
   const [searchText, setSearchText] = useState<string>("");
 
   return (
-    <SafeAreaView
-      className={`flex-1 w-full px-12 ${Platform.OS == "web" && "max-w-2xl mx-auto"}`}
+    <View
+      style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
+      className={`flex-1 w-full ${Platform.OS === "web" && "max-w-2xl mx-auto"}`}
     >
       <Header
-        titleText={category}
+        titleText={name}
         leftButtonText="My Vault"
         leftButtonBackImage={true}
         searchText={searchText}
         onSearchTextChange={setSearchText}
       />
-      <ScrollView className="flex-1 mx-6 my-5">
+      <ScrollView className="flex-1 mx-6">
+        <Spacer size={20} />
         <ThemedText fontSize={12} fontWeight={800}>
           ITEMS (2)
         </ThemedText>
 
         <Spacer size={4} />
 
-        <View className="bg-[#EBEBEB] py-3 px-4 rounded-lg gap-2">
+        <View className="bg-[#EBEBEB] py-4 px-4 rounded-lg gap-2">
           <Pressable className="flex flex-row items-center justify-between">
             <ThemedText fontSize={14}>Login</ThemedText>
             <View className="flex flex-row items-center gap-3">
               <ThemedText fontSize={14}>9</ThemedText>
-              <Image
-                className="max-w-4 max-h-4 rotate-90"
-                source={require("@/assets/images/more.png")}
-              />
+              <MoreIcon width={16} height={16} className="rotate-90" />
             </View>
           </Pressable>
 
@@ -48,14 +50,12 @@ export default function VaultScreen() {
             <ThemedText fontSize={14}>SSH key</ThemedText>
             <View className="flex flex-row items-center gap-3">
               <ThemedText fontSize={14}>12</ThemedText>
-              <Image
-                className="max-w-4 max-h-4 rotate-90"
-                source={require("@/assets/images/more.png")}
-              />
+              <MoreIcon width={16} height={16} className="rotate-90" />
             </View>
           </View>
         </View>
+        <Spacer size={20} />
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 }
