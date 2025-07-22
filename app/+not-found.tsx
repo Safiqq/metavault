7f698acac278d6305a9d96d518a5b49f68fba6cf
safiqq <safiqq@metavault.dev>
2025-07-22 12:00:00 +0700

    feat: create not found page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,36 @@
-import { Link, Stack } from "expo-router";
-import { View, StyleSheet } from "react-native";
+// NotFoundScreen: Shown when a user navigates to a route that doesn't exist.
+import { Link, router, Stack } from "expo-router";
+import { View } from "react-native";
 
+import Spacer from "@/components/Spacer";
 import { ThemedText } from "@/components/ThemedText";
-import React from "react";
+import { ROUTES } from "@/constants/AppConstants";
+import { Button } from "@/components/ui/Button";
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
-      <View style={styles.container}>
-        <ThemedText>This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText>Go to home screen!</ThemedText>
-        </Link>
+      <View className="flex-1 items-center justify-center p-5 bg-white">
+        <ThemedText
+          fontSize={24}
+          fontWeight={700}
+          className="text-center text-black"
+        >
+          Page Not Found
+        </ThemedText>
+        <Spacer size={16} />
+        <ThemedText fontSize={16} className="text-center text-gray-600">
+          This screen does not exist.
+        </ThemedText>
+        <Spacer size={24} />
+        <View className="w-80">
+          <Button
+            text="Go to home screen"
+            onPress={() => router.push(ROUTES.ROOT)}
+          />
+        </View>
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
